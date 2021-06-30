9e77b22322b809341142c357c7991497da0bd7a2
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-06-30 18:26:24 +0100

    add translation to Customer Profile

--- resources/lang/fr/customer.php
+++ resources/lang/fr/customer.php
@@ -3,69 +3,69 @@
 return [
 
     /******************User info***************** */
-    'customer_info_title' => 'معلومات الحساب',
-    'customer_form_name' => 'الاسم',
-    'customer_form_email' => 'البريد الالكتروني',
-    'customer_form_tele' => 'رقم الهاتف',
-    'customer_form_ville' => 'المدينة',
-    'customer_form_address' => 'العنوان',
-    'customer_form_old_pass' => 'كلمة المرور القديمة',
-    'customer_form_new_pass' => 'كلمة المرور الجديدة',
-    'customer_form_new_pass_confirm' => 'تاكيد كلمة المرور الجديدة',
-    'customer_form_update' => 'تحديث المعلومات',
+    'customer_info_title' => 'informations du compte',
+    'customer_form_name' => 'Nom Complet',
+    'customer_form_email' => 'E-mail',
+    'customer_form_tele' => 'numéro de téléphone',
+    'customer_form_ville' => 'Ville',
+    'customer_form_address' => 'adresse',
+    'customer_form_old_pass' => 'ancien mot de passe',
+    'customer_form_new_pass' => 'nouveau mot de passe',
+    'customer_form_new_pass_confirm' => 'confirmer le mot de passe',
+    'customer_form_update' => 'mettre a jour',
 
     /******************END User info***************** */
 
-    'customer_hello' => 'مرحبا',
+    'customer_hello' => 'bienvenue',
 
     /*************************************************** */
 
-    'customer_notification' => 'الاشعارات',
-    'customer_notif_table_title' => 'الاشعار',
-    'customer_notif_table_date' => 'التاريخ',
-    'customer_notif_table_tag' => 'نوع الاشعار',
+    'customer_notification' => 'notifications',
+    'customer_notif_table_title' => 'notification',
+    'customer_notif_table_date' => 'Date',
+    'customer_notif_table_tag' => 'type de notification',
 
     /**************************************************** */
 
-    'customer_orders' => 'الطلبات',
-    'customer_orders_table_id' => 'الرقم',
-    'customer_orders_table_date' => 'التاريخ',
-    'customer_orders_table_total' => 'المجموع',
-    'customer_orders_table_status' => 'الحالة',
-    'customer_orders_table_action' => 'الحدث',
-    'customer_orders_table_view' => 'عرض التفاصيل',
+    'customer_orders' => 'Commandes',
+    'customer_orders_table_id' => 'Numéro',
+    'customer_orders_table_date' => 'Date',
+    'customer_orders_table_total' => 'Total',
+    'customer_orders_table_status' => 'Etat',
+    'customer_orders_table_action' => 'Action',
+    'customer_orders_table_view' => 'voir les détails',
     /*-----------------------------------------------------*/
-    'customer_order' => 'الطلب',
-    'customer_order_product' => 'المنتج',
-    'customer_order_qte' => 'الكمية',
-    'customer_order_price' => 'الثمن',
-    'customer_order_total' => 'المجموع',
-    'customer_order_back' => 'الرجوع',
+    'customer_order' => 'Commande',
+    'customer_order_product' => 'Produit',
+    'customer_order_qte' => 'Quantité',
+    'customer_order_price' => 'Prix',
+    'customer_order_total' => 'Total',
+    'customer_order_back' => 'Retour',
 
-    'customer_order_detail_address' => 'العنوان',
-    'customer_order_detail_name' => 'الاسم',
-    'customer_order_detail_tele' => 'الهاتف',
-    'customer_order_detail_payment' => 'الدفع',
-    'customer_order_detail_payment_method' => 'وسيلة الدفع',
+    'customer_order_detail_address' => 'Adresse',
+    'customer_order_detail_name' => 'Nom',
+    'customer_order_detail_tele' => 'Téléphone',
+    'customer_order_detail_payment' => 'Paiement',
+    'customer_order_detail_payment_method' => 'Méthode de paiement',
     /******************************************************** */
 
-    'customer_address' => 'العناوين',
-    'customer_address_add' => 'اضف عنوان جديد',
-    'customer_address_form_name' => 'الاسم',
-    'customer_address_form_tele' => 'الهاتف',
-    'customer_address_form_email' => 'البريد الاكتروني',
-    'customer_address_form_address' => 'العنوان',
-    'customer_address_form_ville' => 'المدينة',
-    'customer_address_form_add' => 'اضف العنوان',
-    'customer_address_delete' => 'حذف العنوان',
+    'customer_address' => 'Adresses',
+    'customer_address_add' => 'ajouter une nouvelle adresse',
+    'customer_address_form_name' => 'Nom',
+    'customer_address_form_tele' => 'Téléphone',
+    'customer_address_form_email' => 'E-mail',
+    'customer_address_form_address' => 'Adresse',
+    'customer_address_form_ville' => 'Ville',
+    'customer_address_form_add' => "ajouter l'adresse",
+    'customer_address_delete' => "supprimer l'adresse",
 
     /********************************************************* */
 
-    'customer_wishlist' => 'قائمة المفضلات',
-    'customer_whislist_table_name' => 'اسم المنتج',
-    'customer_whislist_table_price' => 'الثمن',
+    'customer_wishlist' => 'Liste de favoris',
+    'customer_whislist_table_name' => 'Produit',
+    'customer_whislist_table_price' => 'Prix',
 
     /****************************************************** */
 
-    'customer_logout' => 'الخروج'
+    'customer_logout' => 'Se deconnecter'
 ];
