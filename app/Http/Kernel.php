5bd347d0aedcf5441eab9d6fad049776b39c5590
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-07-08 23:12:07 +0100

    remove unused files (images , icons ,..) form assets

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
-            \Spatie\ResponseCache\Middlewares\CacheResponse::class,///for cache every response 
+            \Spatie\ResponseCache\Middlewares\CacheResponse::class, ///for cache every response 
         ],
 
         'api' => [
@@ -83,5 +83,7 @@ class Kernel extends HttpKernel
         'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
 
         'doNotCacheResponse'      => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
+        
+        'verifyApiAccess' => \App\Http\Middleware\VerifyAPIAccess::class
     ];
 }
---
+++ app/Http/Middleware/VerifyAPIAccess.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class VerifyAPIAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (
+            !(app()->environment('local'))
+            && (
+                !$request->header('access-token')
+                || $request->header('access-token') !== env('APP_API_TOKEN')
+            )
+        ) {
+            return response()->json(['Message' => 'You do not access to this api.'], 403);
+        }
+
+        return $next($request);
+    }
+}
