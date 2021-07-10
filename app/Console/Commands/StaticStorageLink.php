d1dc0c5910d8ffedad75d2b12926e6c426d9a53c
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-07-10 12:17:52 +0100

    add command to link static disk

---
+++ app/Console/Commands/StaticStorageLink.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StaticStorageLink extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'static:link';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Link static disk';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+         $this->storageLink();
+    }
+
+
+    public function storageLink()
+    {
+        return  symlink(env('FILESYSTEM_STORAGE_ROOT'), env('FILESYSTEM_STORAGE_LINK_PATH'));
+    }
+}
