918b064cfe0091c3011fca78065a36e44586c0fe
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-07-15 00:47:33 +0100

    work in add to cart in Poroducts of section Home

--- resources/views/livewire/cart/add-to-cart.blade.php
+++ resources/views/livewire/cart/add-to-cart.blade.php
@@ -1,12 +1,14 @@
 
-    <li>
-        <a
-             href="#"
-            title="{{__('buttons.add_to_cart')}}"
-            wire:click="addToCartHome({{$prod}})"
-            onclick="return false;"
-        >
-          <i class="icon-bag2"></i>
-        </a>
-    </li>
+   <div>
+    <li  wire:click="addToCartHome({{$prod}})">
+            <a
+                href="#"
+                title="{{__('buttons.add_to_cart')}}"
+            
+                onclick="return false;"
+            >
+            <i class="icon-bag2"></i>
+            </a>
+        </li>
+   </div>
 
