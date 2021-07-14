c970609dabfa2ef6a02dfa8bf6cdb7fde2d0ae7b
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-07-14 04:13:05 +0100

    add explore navbar link

--- resources/views/theme/explore/index.blade.php
+++ resources/views/theme/explore/index.blade.php
@@ -6,11 +6,12 @@
 
 <div class="ps-page--shop">
     <div class="ps-container">
+        <h1>Explore functionality : TEST Mode</h1>
 
         @include('theme.explore.section_b_banner')
         @include('theme.explore.section_c_brand')
         @include('theme.explore.section_d_products')
-        
+
     </div>
 </div>
 
