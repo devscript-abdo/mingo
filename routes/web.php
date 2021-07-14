42a52530acd277ff4eb5b869b820a021f79154ea
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-07-14 13:58:13 +0100

    add new routes dev

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use App\Http\Controllers\ProductCollectionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -176,6 +176,37 @@ Route::group(['prefix' => config('mingo.admin')], function () {
     Route::delete('/attr/delete', [AttributeController::class, 'destroy'])->name('admin.attrs.delete');
 
     Route::get('/attrs/{slug}', [AttributeController::class, 'getC'])->name('admin.attrs.get');
+
+    Route::group(['prefix' => 'dev'], function () {
+
+        Route::get('/sitemap', function () {
+            Artisan::call('sitemap:generate');
+        });
+
+        Route::get('/optimize', function () {
+            Artisan::call('optimize');
+        });
+
+        Route::get('/cache-config', function () {
+            Artisan::call('config:cache');
+        });
+        Route::get('/cache-route', function () {
+            Artisan::call('route:cache');
+        });
+        Route::get('/cache-view', function () {
+            Artisan::call('view:cache');
+        });
+
+        Route::get('/clear-config', function () {
+            Artisan::call('config:clear');
+        });
+        Route::get('/clear-route', function () {
+            Artisan::call('route:clear');
+        });
+        Route::get('/clear-view', function () {
+            Artisan::call('view:clear');
+        });
+    });
 });
 
 //Auth::routes();
