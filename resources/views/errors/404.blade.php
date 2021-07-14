1467c5e47b6b39abfbe8f7fe674a41fa56e551d9
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-07-14 06:54:06 +0100

    change 404 page style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,23 @@
-@extends('errors::minimal')
+{{--@extends('errors::minimal')
 
 @section('title', __('Not Found'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('Not Found'))--}}
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="ps-page--404">
+        <div class="container">
+            <div class="ps-section__content"><img src="{{asset('assets/img/404.jpg')}}" alt="Mingo.ma">
+                <h3>ohh! page not found</h3>
+                <p>It seems we can't find what you're looking for. Perhaps searching can help or go back to
+                    <a href="{{route('home')}}"> {{__('navbar.home')}}</a></p>
+                <form class="ps-form--widget-search" action="" method="get">
+                    <input class="form-control" type="text" placeholder="Search...">
+                    <button><i class="icon-magnifier"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
