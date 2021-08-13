f7a70d8d5080669b20755d561f41d7648cb0ec25
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-08-13 17:22:09 +0100

    change category Structure by adding Tree function to get category with  they chidlrens without overloading Queries

--- resources/views/theme/products/default/section_left_a_categories.blade.php
+++ resources/views/theme/products/default/section_left_a_categories.blade.php
@@ -2,7 +2,7 @@
     <h4 class="widget-title">{{__('products.product_categories')}}</h4>
     <ul class="ps-list--categories">
         @foreach($categories as $categorie)
-            @if(count($categorie->nestedChilds))
+            @if(!is_null($categorie->nestedChilds))
                 <li class="menu-item-has-children" >
                     <a href="{{$categorie->url}}">{{$categorie->field('name')}}</a>
                     <span class="sub-toggle"><i class="fa fa-angle-down"></i></span>
