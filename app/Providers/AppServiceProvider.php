bf25413bd79bf882d52d206492db35f746cfae1f
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-08-24 16:21:05 +0100

    Fix issues in Admins table migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
         //
         // using the laravel voyager dashborad->add team (facebook, linked,....)
         Validator::extend('validateDomain', function ($attribute, $value, $parameters, $validator) {
